93ef0c7a71b867d07f5df600f803bb43d1792579
Om-Kumar-Ace <om2005kumar001@gmail.com>
2023-12-31 12:14:53 +0530

    day 11 in python

---
+++ Day 11 .py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+def hourglass_sum(arr):
+    max_sum = float('-inf')  
+    for i in range(4): 
+        for j in range(4): 
+            current_sum = (
+                arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                arr[i + 1][j + 1] +
+                arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+            )
+            max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+if __name__ == '__main__':
+    
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    result = hourglass_sum(arr)
+    print(result)
+    

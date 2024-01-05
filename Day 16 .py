5ca347b69cbace000f8864927ec5e409bb57c7af
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-05 20:21:23 +0530

    Day 16 in python

--- Day 16 .py
+++ Day 16 .py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -5,17 +7,10 @@ import re
 import sys
 
 
-if __name__ == '__main__':
-    try:
-        # Read input from the user
-        S = input().strip()
 
-        # Try to convert the string to an integer
-        integer_value = int(S)
-
-        # If successful, print the integer value
-        print(integer_value)
-
-    except ValueError:
-        # If conversion fails, print "Bad String"
-        print("Bad String")
+S = input()
+try:
+    integer = int(S)
+    print(integer)
+except ValueError:
+    print('Bad String')

49486b4b17b1fc9cd6b6db0d562191de028e900f
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-07 19:31:32 +0530

    removing commits

--- Day 18 .py
+++ Day 18 .py
@@ -17,13 +17,11 @@ class Solution:
         
     def dequeueCharacter(self):
         return self.queue.pop(0)
-# read the string s
 s=input()
-#Create the Solution class object
 obj=Solution()   
 
 l=len(s)
-# push/enqueue all the characters of string s to stack
+
 for i in range(l):
     obj.pushCharacter(s[i])
     obj.enqueueCharacter(s[i])
@@ -38,7 +36,6 @@ for i in range(l // 2):
     if obj.popCharacter()!=obj.dequeueCharacter():
         isPalindrome=False
         break
-#finally print whether string s is palindrome or not.
 if isPalindrome:
     print("The word, "+s+", is a palindrome.")
 else:
